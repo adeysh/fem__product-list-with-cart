11188edf82ba0c62d7dd11def960b9ba0753a0bb
Adesh Katiya <adeshkatiya27@gmail.com>
2025-12-06 00:38:58 +0530

    add attribution

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import ProductsPage from "./pages/ProductsPage";
 import { CartProvider } from "./context/CartProvider";
+import Footer from "./components/Footer";
 
 function App() {
     return (
-        // Global Cart context for the whole application
-        <CartProvider>
-            {/* Main page layout */}
-            <ProductsPage />
-        </CartProvider>
+        <>
+            {/* Global Cart context for the whole application */}
+            <CartProvider>
+                {/* Main page layout */}
+                <ProductsPage />
+            </CartProvider>
+
+            <Footer />
+        </>
     );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <div class="attribution">
+            Challenge by{" "}
+            <a href="https://www.frontendmentor.io?ref=challenge">
+                Frontend Mentor
+            </a>
+            . Coded by{" "}
+            <a href="https://www.frontendmentor.io/profile/adeysh">
+                Adesh Katiya
+            </a>
+            .
+        </div>
+    );
+};
+
+export default Footer;
